28fc85229bd38e60a85f38e1b49189efa018796f
Anthony Rurup <tcrurup@gmail.com>
2019-07-17 23:14:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,9 +5,8 @@ class StudentsController < ApplicationController
   end
 
   def new
-    Student.create(first_name: params[:first_name], last_name: params[:last_name]).tap do |x|
-      redirect_to student_path(x)
-    end
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(x)
   end
 
   def show
