a5a4fe13960bf9780039c0b0c4678d9cd0f877a2
Anthony Rurup <tcrurup@gmail.com>
2019-07-17 23:55:34 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,9 @@
 class StudentsController < ApplicationController
 
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name]).tap |s| do
+
+    end
   end
 
   def index
